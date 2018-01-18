0697a761436ff1895d3829e95d772618a5be486d
minerstat <hello@coinscrow.io>
2018-01-18 12:52:42 +0100

    AutoUpdater --rebase

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 git config --global user.email "test@test.com"
 git config --global user.name "test"
-git pull
+git pull --rebase
 git add * -f
 git commit -a -m "AutoUpdate"
-git pull
+git pull --rebase

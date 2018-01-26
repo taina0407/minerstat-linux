f51708e796e59fe4dca245d64e2ac3bbc2b3772a
minerstat <hello@coinscrow.io>
2018-01-26 16:01:58 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/bash
-git config --global user.email "test@test.com"
-git config --global user.name "test"
-git pull 
-git add * -f
-git commit -a -m "AutoUpdate"
+git config --global user.email "dump@minerstat.com"
+git config --global user.name "minerstat"
 git pull

c68378da19dfc9d0305c0b07ce685e049b99b704
minerstat <hello@coinscrow.io>
2018-01-28 16:47:45 +0100

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -2,21 +2,23 @@ if ! screen -list | grep -q "dummy"; then
 
 screen -A -m -d -S dummy sleep 86400
 
+echo "boot" > random.txt
+
 echo ""
-echo "-------- INITALIZING FAKE DUMMY PLUG ------------"
+echo "-------- INSTALLING FAKE DUMMY PLUG ------------"
 echo "Please wait.."
 sleep 1
 sudo update-grub
 sudo nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=28 --use-display-device="DFP-0" --connected-monitor="DFP-0" --enable-all-gpus
 export XAUTHORITY="$XDG_CACHE_HOME/Xauthority"
-sudo service lightdm start
-sleep 10
+#sudo service lightdm start
+#sleep 10
 sudo chvt 1
 echo ""
 
-echo "-------- OVERCLOCKING ---------------------------"
-cd /home/minerstat/minerstat-linux/bin
-sudo sh overclock.sh
+#echo "-------- OVERCLOCKING ---------------------------"
+#cd /home/minerstat/minerstat-linux/bin
+#sudo sh overclock.sh
 
 echo " "
 echo "-------- AUTO CONFIGURE NETWORK ADAPTERS --------"

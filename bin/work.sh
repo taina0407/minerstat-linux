bc4d727faee92343512bf2f609f8c5a9e70f0793
minerstat <hello@coinscrow.io>
2018-01-28 16:50:16 +0100

    Create work.sh

---
+++ bin/work.sh
@@ -0,0 +1,49 @@
+if ! screen -list | grep -q "dummy"; then
+
+screen -A -m -d -S dummy sleep 86400
+
+echo ""
+echo "-------- INIZALIZING FAKE DUMMY PLUG -------------"
+echo "Please wait.."
+sleep 1
+sudo service lightdm start
+sleep 10
+echo ""
+
+echo "-------- OVERCLOCKING ---------------------------"
+cd /home/minerstat/minerstat-linux/bin
+sudo sh overclock.sh
+
+echo " "
+echo "-------- AUTO CONFIGURE NETWORK ADAPTERS --------"
+cd /home/minerstat/minerstat-linux/bin
+sudo sh dhcp.sh
+
+sleep 2
+
+echo "-------- WAITING FOR CONNECTION -----------------"
+echo ""
+
+while ! sudo ping minerstat.com -w 1 | grep "0%"; do
+sleep 1
+done
+
+echo ""
+echo "-------- AUTO UPDATE MINERSTAT ------------------"
+echo ""
+cd /home/minerstat/minerstat-linux
+sudo sh git.sh
+echo ""
+
+echo "-------- RUNNING JOBS ---------------------------"
+cd /home/minerstat/minerstat-linux/bin
+sudo sh jobs.sh
+echo ""
+
+echo "-------- INITALIZING MINERSTAT CLIENT -----------"
+cd /home/minerstat/minerstat-linux
+screen -A -m -d -S minerstat-console sh /home/minerstat/minerstat-linux/start.sh;
+echo ""
+echo "Minerstat has been started in the background.."
+echo "minerstat-console to display output.."
+fi

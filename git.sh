f1b500dbed198f567d012c4b3a9be4ad82d63f94
minerstat <hello@coinscrow.io>
2018-02-05 12:36:10 +0100

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,15 @@
 #! /usr/bin/bash
 git config --global user.email "dump@minerstat.com"
 git config --global user.name "minerstat"
-git pull --no-edit
+RESPONSE="$(git pull --no-edit)"
+
+sleep 1
+
+if echo "$RESPONSE" | grep -iq "^commit your changes" ;then
+
+sleep 2
+
+sudo git commit -a -m "Init"
+sudo git merge
+
+fi

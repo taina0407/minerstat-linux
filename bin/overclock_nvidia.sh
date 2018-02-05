cf87e81867157b5cadb85b4e29a31276181cb3f1
minerstat <hello@coinscrow.io>
2018-02-05 22:55:28 +0100

    Final oc 1/2

--- bin/overclock_nvidia.sh
+++ bin/overclock_nvidia.sh
@@ -25,7 +25,10 @@ MEMORYOFFSET=$4
 COREOFFSET=$5
 
 sudo nvidia-smi -pm 1
+if [ "$POWERLIMITINWATT" -ne 0 ]
+then
 sudo nvidia-smi -i $GPUID -pl $POWERLIMITINWATT
+fi
 sudo nvidia-settings -c :0 -a '[gpu:'"$GPUID"']/GPUFanControlState=1' -a '[fan:0]/GPUTargetFanSpeed='"$FANSPEED"'';
 sudo nvidia-settings -c :0 -a '[gpu:'"$GPUID"']/GPUMemoryTransferRateOffset[3]='"$MEMORYOFFSET"''
 sudo nvidia-settings -c :0 -a '[gpu:'"$GPUID"']/GPUGraphicsClockOffset[3]='"$COREOFFSET"''

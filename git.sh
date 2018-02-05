45fe8b0eb89f9f687bee7ec0c20e729473c3e0ae
minerstat <hello@coinscrow.io>
2018-02-05 13:19:51 +0100

    Final AutoUpdater script

--- git.sh
+++ git.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
+exec 2>/home/minerstat/debug.txt
 git config --global user.email "dump@minerstat.com"
 git config --global user.name "minerstat"
 RESPONSE="$(git pull --no-edit)"
@@ -7,13 +8,15 @@ echo "$RESPONSE"
 
 sleep 1
 
-if echo "$RESPONSE" | grep -q "merge"; then
+if grep -q "merge" /home/minerstat/debug.txt; then
 
 sleep 2
 
 sudo git commit -a -m "Init"
-sudo git merge
+sudo git merge --no-edit
 sudo git add * -f
 sudo git commit -a -m "Fix done"
 
 fi
+
+sudo rm /home/minerstat/debug.txt

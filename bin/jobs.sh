60c598d5038f24011890bebfb0d8bb34f21087b4
minerstat <hello@coinscrow.io>
2018-02-19 13:43:10 +0100

    Update jobs.sh

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+exec 2>/dev/null
+echo "Running Clean jobs.."
 find '/home/minerstat/minerstat-linux' -name "*log.txt" -type f -delete
 echo "Log files deleted"
 sudo dmesg -n 1
 sudo apt clean
+#Deleting Old Kernels
+sudo rm /boot/initrd.img-4.10.0-42-lowlatency > /dev/null
+sudo rm /boot/initrd.img-4.10.0-42-generic.old-dkms > /dev/null
